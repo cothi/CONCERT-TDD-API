2b07e92fa03a8e0c184bf5d458f8c1a7cdda1dd6
cothi <jiungdev@gmail.com>
2024-06-16 21:30:54 +0900

    feat: 포인트 충전 기능 (HTTP PATCH METHOD)

--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { UserPointTable } from "../database/userpoint.table";
 import { PointHistoryTable } from "../database/pointhistory.table";
-import { PointHistory, UserPoint } from "./point.model";
+import { PointHistory, TransactionType, UserPoint } from "./point.model";
+import { PointBody } from "./point.dto";
 
 @Injectable()
 export class PointService {
@@ -16,5 +17,19 @@ export class PointService {
 
   async getPointHistoryByUserId(userId: number): Promise<PointHistory[]> {
     return await this.historyDb.selectAllByUserId(userId);
+  }
+
+  async chargePoint(userId: number, pointDto: PointBody): Promise<UserPoint> {
+    const userPoint = await this.userDb.selectById(userId);
+    const newPoint = userPoint.point + pointDto.amount;
+    await this.userDb.insertOrUpdate(userId, newPoint);
+    await this.historyDb.insert(
+      userId,
+      pointDto.amount,
+      TransactionType.CHARGE,
+      Date.now()
+    );
+
+    return await this.userDb.selectById(userId);
   }
 }
